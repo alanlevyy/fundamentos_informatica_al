b31c62c796b5160abba8f27e71ca8d07ee1b459e
alanlevyy <alanlevy2009@hotmail.com>
2022-04-18 10:21:48 -0300

    agregue practica 1 de python intro

---
+++ Python_intro/Practicas/practica1.py
@@ -0,0 +1,33 @@
+from calendar import c
+from re import A
+
+#ej1 
+
+#A = input ("Ingresar Texto Largo:")
+#print (len(A))
+
+#ej2
+
+#B = input ("Ingresa texto:")
+#print (B[5] + B[6])
+
+#ej3
+
+#C = input ("¿cual es tu nombre y apellido?")
+#print ("hola " + C)
+
+#ej4
+
+#D1 = input ("¿cual es tu nombre?")
+#D2 = input ("cual es tu 1er apellido?")
+#D3 = input ("cual es tu 2do apellido?")
+#print (str.upper(D1[0] + D2[0] + D3[0]))
+
+
+## ej5 
+programa = input ("dame un numero:") 
+programa1= input ("dame otro numero:") 
+programa2= input ("dame otro:")
+
+promedio= (int(programa) + int(programa1) + int(programa2)/3)
+print (promedio)
