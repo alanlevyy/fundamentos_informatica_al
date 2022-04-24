56462d5d38f16b89bb443b6afb676edf27e1e12d
alanlevyy <alanlevy2009@hotmail.com>
2022-04-24 17:39:02 -0300

    agrego mas practica

---
+++ Python_intro/Practicas/manipulacion.py
@@ -0,0 +1,14 @@
+#### ej1
+#with open('archivo_practica_manipulacion', 'r') as file: 
+  #  fileContent = file.readlines()
+
+#for fileline in fileContent: 
+   # noEmpiezaCon = 0 
+   # if not str.startswith(fileline, 'P'): 
+   #     noEmpiezaCon += 1
+   # print(noEmpiezaCon) 
+
+
+### ej2
+
+        
