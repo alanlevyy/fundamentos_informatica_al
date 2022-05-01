71491905e87e1d723fef1f55c0c12421b7bbae03
alanlevyy <alanlevy2009@hotmail.com>
2022-05-01 19:08:42 -0300

    practica

---
+++ Python_intro/datos_personales/PRACTICANDO.py
@@ -0,0 +1,132 @@
+#lista = ["hpña", "sosos",4]
+
+#print (lista)
+
+#print (lista[0])
+
+#print (lista.append('22'))
+
+#print (lista)
+#print (lista.index('hpña'))
+
+
+#diccionario = {"sorete": "sos gil", "sos bolo": "trolo"}
+
+
+#print (diccionario)
+#print (diccionario.keys())
+
+#print(diccionario.keys())
+
+
+
+
+#iccionario = {1: 1*2 , 2: 2*2, 3 : 3*2 }
+
+#print(diccionario)
+
+#def doble(numero):
+ #   resultado = numero * 2
+  #  return resultado
+
+# print (doble(4))
+
+
+
+
+
+
+
+
+#def termos_x_ronda (personas):
+   # resultado = personas*30/1000
+   # return resultado
+
+  
+#print(termos_x_ronda(10))
+
+
+#def vaquita (costos, personas):
+   # resultado = costos/personas
+   # return resultado
+
+#print (vaquita(10, 10))
+
+
+#def calcular_cant_agua (personas):#
+   # resultado = personas*30
+  #  if resultado < 1000:
+   #    print(resultado) 
+   # else:
+     #     print ("vas a necesitar más de un térmo")
+
+
+#calcular_cant_agua(40)
+
+from xml.dom.expatbuilder import ElementInfo
+
+
+pedido = {"Ana": "no_veggie", "Paul": "veggie", "Luz": "veggie"}
+
+
+
+lista_comensales = pedido.keys()
+
+#gustos = {"veggie": 0, "no_veggie": 0}
+
+#def empanadas_x_gusto():
+    #for i in lista_comensales:
+       #print(pedido[i])
+      
+#for a in lista_comensales:
+    #gustos[pedido[a]] +=1
+
+
+#print (gustos)
+
+# repite el bucle en todas las partes del diccionario o lista 
+# la i esta recorriendo ami lista elemento x Elemento
+coleccion = {"ana": "veggie", "juan": "no veggie"}
+def señor ():
+ for i in coleccion :
+   print ("gool")
+
+print (señor())
+
+
+# from tkinter import W
+#print (os.getcwd())
+
+#print (os.chdir('/Users/alanlevy/Desktop/fund_infopractica/fundamentos_informatica_al/Python_intro/Practicas'))
+# path = '/Users/alanlevy/Desktop/fund_infopractica/fundamentos_informatica_al/Python_intro/soyyo.txt'
+
+#print (os.getcwd())
+
+# with open (path, 'w') as my_file:
+   # my_file.write ("sos cagon")
+
+#open().read()
+
+#print(os.getcwd())
+# path = '/Users/alanlevy/Desktop/fund_infopractica/fundamentos_informatica_al/Python_intro/datos_personales/user'
+#
+#os.mkdir(path,1)
+
+#os.chdir('/Users/alanlevy/Desktop/fund_infopractica/fundamentos_informatica_al/Python_intro/datos_personales')
+
+#with open (path,'w') as user:
+#   user.write(" Alan Levy y tengo 20 años")
+
+
+# bio = open("user", "r")
+# print(bio.readlines())
+
+#def mitades (numero):
+  # print(numero/2)
+
+
+#mitades(0)
+
+
+
+
